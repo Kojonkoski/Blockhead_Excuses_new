b6a4e249570692c373a086fe23e02111197170bd
Janne Sokka <janne.sokka@aitoo.fi>
2024-03-20 22:53:28 +0200

    CharacterController goes oon

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -9,9 +9,15 @@ public class MovementController : MonoBehaviour
     float moveSpeed;
     public float walkSpeed, runSpeed;
     GameObject playerCharacter;
+    bool isRunningKey, isRunningStick, isRunning;
+    bool isGrounded;
+    public float groundCheckDistance = 0.01f;
+    public LayerMask groundLayer;
+    Animator anim;
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
         playerCharacter = GameObject.FindWithTag("Player");
     }
 
@@ -19,14 +25,35 @@ public class MovementController : MonoBehaviour
     {
         playerInput = _context.ReadValue<Vector2>();
     }
-
+    public void Running(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            isRunningKey = true;
+        }
+        if (_context.performed)
+        {
+            isRunningKey = true;
+        }
+        if (_context.canceled)
+        {
+            isRunningKey = false;
+        }
+    }
 
     public void Update() 
     {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundLayer);
         MovePlayer();
 
         if (playerInput != Vector2.zero) {
             moveSpeed = walkSpeed;
+        }
+        if (isRunningKey || isRunningStick) {
+            isRunning = true;
+        }
+        else if (!isRunningKey && !isRunningStick) {
+            isRunning = false;
         }
     }
 
@@ -34,6 +61,27 @@ public class MovementController : MonoBehaviour
     {
         Vector3 moveVector = playerInput.x * Vector3.right + playerInput.y * Vector3.forward;
         moveVector.Normalize();
+
+        if (isGrounded && moveVector !=Vector3.zero) 
+        {
+            if (isRunning)
+            {
+                moveSpeed = runSpeed;
+                anim.SetTrigger("run");
+            }
+            if (!isRunning)
+            {
+                moveSpeed = walkSpeed;
+                anim.SetTrigger("walk");
+            }
+        }
+        
+
+        if(moveVector == Vector3.zero)
+        {
+            anim.SetTrigger("idle");
+        }
+
         playerCharacter.transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
     }
 
