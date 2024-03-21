d0565d249f3e988c395c71a1eb71d7b436fafdca
Kojonkoski <128294860+Kojonkoski@users.noreply.github.com>
2024-03-21 12:21:27 +0200

    Commit21032024_1
    Gamemanager and some animations

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
+public class GameManager : MonoBehaviour
+{
+    public float transitionTime = 1f;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadLevel(1));
+    }
+
+    public void Mainmenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public IEnumerator LoadLevel(int levelToLoad)
+    {
+        animator.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
